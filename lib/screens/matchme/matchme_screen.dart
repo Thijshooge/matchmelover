83a1ee493cf77137a46986a0ce4c0b85a33236a5
Thijshooge <thyshooge@gmail.com>
2025-06-27 23:06:05 +0200

    Auto commit

--- lib/screens/matchme/matchme_screen.dart
+++ lib/screens/matchme/matchme_screen.dart
@@ -33,7 +33,7 @@ class MatchMeScreen extends StatelessWidget {
         actions: [
           IconButton(
             onPressed: () {
-              Navigator.pushNamed(context, '/matchme/push_notifications');
+              Navigator.pushNamed(context, '/matchme/push_notification');
             },
             padding: EdgeInsets.symmetric(horizontal: 4),
             constraints: BoxConstraints(),
